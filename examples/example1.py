1d830a27666b9722655a43a71665e71f24a2745d
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-05-29 12:43:44 +0200

    Documented example1

--- examples/example1.py
+++ examples/example1.py
@@ -2,11 +2,24 @@
 
 from linearizator import linearizator
 
+# Initialize linearizator object
 lin = linearizator.linearizator(x1=6.4, y1=20, x2=16.8, y2=85, unknown="y")
+
+# Linearize
 lin.linearize()
+
+# Show equation of line
 print(lin.pretty_equation())
+
+# Calculate the value of a point
 print(lin.calculate(6.4))
+
+# Generate inverse linearization
 lin.inverse()
+
+# Show equation of line
 print(lin.pretty_equation())
+
+# Calculate the value of a point
 print(lin.calculate(20))
 
