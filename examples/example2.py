67c457f04455a4fe0ce3262198643d6d2e47cb51
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-05-29 12:10:17 +0200

    Improved example2

--- examples/example2.py
+++ examples/example2.py
@@ -2,10 +2,15 @@
 
 from linearizator import linearizator
 
-lin2 = linearizator.linearizator(x1=8, y1=30, x2=17, y2=84, unknown="y")
-lin2.linearize()
-print(lin2.pretty_equation())
-print(lin2.calculate(17))
-lin2.inverse()
-print(lin2.pretty_equation())
-print(lin2.calculate(84))
+lin = linearizator.linearizator(x1=8, y1=30, x2=17, y2=84, unknown="y")
+lin.x1 = 8
+lin.y1 = 30
+lin.x2 = 17
+lin.y2 = 84
+lin.unknown = "y"
+lin.linearize()
+print(lin.pretty_equation())
+print(lin.calculate(17))
+lin.inverse()
+print(lin.pretty_equation())
+print(lin.calculate(84))
